8365072d165886af5fe96acae3ba4886870cebf1
AdrianChwialkowski <adrian.chwialkowski1@gmail.com>
2018-10-20 11:53:24 +0200

    +1+3

--- src/Test2.java
+++ src/Test2.java
@@ -2,5 +2,8 @@ public class Test2 {
     public static void main(String[] args) {
         String test = "Fuuuck";
         String test2 =  "What the ";
+        System.out.println(test2 + test);
+
+        System.out.print("Why mister Anderson");
     }
 }
